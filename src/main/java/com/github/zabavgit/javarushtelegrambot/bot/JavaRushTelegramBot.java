5f0b863e0ae92700efd8cb3b8a47cc2524b3e61a
zabava <zabavgit@gmail.com>
2026-07-07 20:24:05 +0300

    JRTB-3: Implement Command pattern for telegrambot

---
+++ src/main/java/com/github/zabavgit/javarushtelegrambot/bot/JavaRushTelegramBot.java
@@ -0,0 +1,48 @@
+package com.github.zabavgit.javarushtelegrambot.bot;
+
+import com.github.zabavgit.javarushtelegrambot.command.CommandContainer;
+import com.github.zabavgit.javarushtelegrambot.command.CommandName;
+import com.github.zabavgit.javarushtelegrambot.service.SendBotMessageServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class JavaRushTelegramBot extends TelegramLongPollingBot {
+    public static String COMMAND_PREFIX = "/";
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    private final CommandContainer commandContainer;
+    public JavaRushTelegramBot() {
+        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            if (message.startsWith(COMMAND_PREFIX)) {
+                String commandIdentifier = message.split(" ")[0].toLowerCase();
+
+                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+            } else {
+                commandContainer.retrieveCommand(CommandName.NO.getCommandName()).execute(update);
+            }
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
